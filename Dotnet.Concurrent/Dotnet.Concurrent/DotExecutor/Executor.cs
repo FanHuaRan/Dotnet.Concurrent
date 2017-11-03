1e9e3c080df024d61ea299e239bbafc17859ff31
FanHuaRan <834171100@qq.com>
2017-11-03 18:07:14 +0800

    2017/11/03 fhr Exectors

---
+++ Dotnet.Concurrent/Dotnet.Concurrent/DotExecutor/Executor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dotnet.Concurrent.DotExecutor
+{
+    /// <summary>
+    /// 线程池基本接口
+    /// 2017//11/03 fhr
+    /// </summary>
+    public interface Executor
+    {
+        void Execute(Action command);
+    }
+}
---
+++ Dotnet.Concurrent/Dotnet.Concurrent/DotExecutor/Executors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dotnet.Concurrent.DotExecutor
+{
+    /// <summary>
+    /// 线程池工厂类
+    /// 2017//11/03 fhr
+    /// </summary>
+    public class Executors
+    {
+
+    }
+}
